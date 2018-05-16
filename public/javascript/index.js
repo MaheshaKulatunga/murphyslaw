a5d6b232a7f01345eda4ad63809442705ebcad2c
Mahesha Kulatunga <mkkulatunga1@sheffield.ac.uk>
2018-05-16 20:02:05 +0530

    Company page updates

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -4,23 +4,29 @@ function parallax() {
   setpos("#bg3", 4);
   setpos("#bg4");
   setpos("#bg5");
+  setpos("#bg6");
+  setpos("#bg7");
+
 }
 
 
 
 function setpos(element, factor) {
-  factor = factor || 2;
+  if ($(element).offset() != undefined){
+    factor = factor || 2;
 
-  var offset = $(element).offset();
-  var w = $(window);
+    var offset = $(element).offset();
+    var w = $(window);
 
-  var posx = (offset.left - w.scrollLeft()) / factor;
-  var posy = (offset.top - w.scrollTop()) / factor;
+    var posx = (offset.left - w.scrollLeft()) / factor;
+    var posy = (offset.top - w.scrollTop()) / factor;
 
-  $(element).css('background-position', '50% ' + posy + 'px');
+    $(element).css('background-position', '50% ' + posy + 'px');
 
-  // use this to have parralax scrolling vertical and horizontal
-  //$(element).css('background-position', posx+'px '+posy+'px');
+    // use this to have parralax scrolling vertical and horizontal
+    //$(element).css('background-position', posx+'px '+posy+'px');
+  }
+
 }
 
 $(document).ready(function () {
